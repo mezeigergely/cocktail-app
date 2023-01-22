0922b1dfe3d60c0039a3a945cf710d0e316c1493
mezeigergely <mezei.gergely89@gmail.com>
2023-01-22 21:17:10 +0100

    added constants file into config

---
+++ config/constants.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "api_url" => "https://www.thecocktaildb.com/api/json/v1/1/",
+    "random_cocktail" => "random.php",
+    "search_by_name" => "search.php?s=",
+    "non_alcoholic" => "filter.php?a=Non_Alcoholic"
+];
